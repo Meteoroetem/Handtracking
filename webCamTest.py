d15e4d3de9f86d2a91250f56223ebc5dee74cb9e
Meteoroetem-laptop <rotem.kassif@gmail.com>
2024-06-17 02:45:04 +0300

    Added more compatibility (checks two cameras)

--- webCamTest.py
+++ webCamTest.py
@@ -5,7 +5,9 @@ from mediapipe.tasks.python import vision
 
 
 cv.namedWindow("CamWindow", cv.WINDOW_NORMAL)
-capture = cv.VideoCapture(1)
+capture = cv.VideoCapture(0)
+if not capture.isOpened():
+    capture = cv.VideoCapture(1)
 
 while (k := cv.waitKey(1)) != ord('e'):
     returned, frame = capture.read()
